ae05777eb5904a01d8865ff3b90b8fda4d83f9a3
Deepak D Gowda <100547880+Deepak-D-Gowda@users.noreply.github.com>
2023-07-06 07:10:13 +0530

    Add files via upload

---
+++ EvenNumber_OddNumber/EvenOrOdd.java
@@ -0,0 +1,18 @@
+package even;
+
+public class EvenOrOdd {
+	public static void main(String[] args) {
+		//  Logic for Odd Number is ---> if(num%2!=0) 
+		
+		int num = 3;
+		
+		if(num%2==0)
+		{
+			System.out.println("Even Number");
+		}
+		else
+		{
+			System.out.println("Odd Number");
+		}
+	}
+}
---
+++ Prime_Number/PrimeNumbers.java
@@ -0,0 +1,31 @@
+package primenumber;
+
+
+
+public class PrimeNumbers {
+public static void main(String[] args) {
+	
+	int num = 10;
+	int count = 0;
+	
+	if(num>1)
+	{
+		for(int i=1; i<=num; i++)
+		{
+			if(num%i==0)
+			{
+				count++;
+			}
+		}
+	}
+	
+		if(count==2)
+		{
+			System.out.println("Prime number");
+		}
+		else
+		{
+			System.out.println("not a Prime Number");
+		}
+}
+}
